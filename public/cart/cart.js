4465ab9524480c6683c204f9fe8add655016303e
Shashank <shashank_smaty@yahoo.co.in>
2016-11-20 11:43:48 +0530

    Changes made to cart

--- public/cart/cart.js
+++ public/cart/cart.js
@@ -11,6 +11,33 @@ angular.module('cart', ['ngRoute'])
 
 	$http.get("public/list.json").then(function(response){
 		$scope.cartData = response.data;
+		console.dir($scope.cartData);
 	});
 
+	$scope.total = function(){
+		var t = 0;
+		for(var k in $scope.cartData) {
+			t += parseInt($scope.cartData[k].selected);
+		}
+		return t;
+	};
+
 }])
+
+.directive('checkList', function(){
+	return {
+		restrict: 'E',
+		scope: {
+			option: '=',
+			name: '=',
+			selected: '='
+		},
+		template: function(elem, attrs) {
+			return '<div class="panel-body">\
+						<div class="radio" ng-repeat="i in option">\
+							<label><input type="radio" ng-model="$parent.selected" name="{{name}}" ng-value="{{i.price}}">{{i.size}}, Rs.{{i.price}} {{selected}}-{{selected}}</label>\
+						</div>\
+					</div>'
+		}
+	};
+})
